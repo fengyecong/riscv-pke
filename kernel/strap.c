7477b6c2c7bee7cfe611c0ae9c64c4ae60dd3442
fengyecong <fengyecong@163.com>
2023-05-17 10:31:54 +0800

    my work on lab2_3 is done.

--- kernel/strap.c
+++ kernel/strap.c
@@ -9,6 +9,7 @@
 #include "pmm.h"
 #include "vmm.h"
 #include "util/functions.h"
+#include "memlayout.h"
 
 #include "spike_interface/spike_utils.h"
 
@@ -56,13 +57,18 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
   sprint("handle_page_fault: %lx\n", stval);
   switch (mcause) {
     case CAUSE_STORE_PAGE_FAULT:
+    {
       // TODO (lab2_3): implement the operations that solve the page fault to
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      void *pa=alloc_page();
+      if (pa==0) panic("user men alloc falied\n");
+      user_vm_map(current->pagetable, stval, 1, (uint64)pa, prot_to_type(PROT_WRITE|PROT_READ, 1));
 
       break;
+    }
     default:
       sprint("unknown page fault.\n");
       break;
